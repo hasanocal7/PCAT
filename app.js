64e7802fe71b04046e65336d0f24d3678fbca8fa
hasan <hasanocal7@gmail.com>
2023-09-19 22:02:30 +0300

    Update Photos Action and Edited CSS

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+// Modules
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 
+// Create a App
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // MiddleWare
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos });
 });
 
 app.get('/about', (req, res) => {
@@ -25,9 +36,10 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
-  res.redirect('/')
+// Action
+app.post('/photos', async (req, res) => {
+  Photo.create(req.body);
+  await res.redirect('/');
 });
 
 // Port
